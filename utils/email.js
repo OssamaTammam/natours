5fc11ac153d8d2d0e943f87a38942988439562bc
Ossama Tammam <ossamatammam02@gmail.com>
2023-09-28 23:33:42 +0300

    Small change

--- utils/email.js
+++ utils/email.js
@@ -11,9 +11,10 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === "production") {
-      return 1;
-    }
+    // TODO: Send emails
+    // if (process.env.NODE_ENV === "production") {
+    //   return 1;
+    // }
 
     return nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
